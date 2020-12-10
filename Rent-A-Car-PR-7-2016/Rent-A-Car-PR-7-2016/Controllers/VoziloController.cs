ef698ebe990ea9556124f77f05b468df26373110
Marko Sakotic <marcodeee@gmail.com>
2020-12-10 21:14:28 +0100

    add/car-controller-and-view

---
+++ Rent-A-Car-PR-7-2016/Rent-A-Car-PR-7-2016/Controllers/VoziloController.cs
@@ -0,0 +1,37 @@
+﻿using Rent_A_Car_PR_7_2016.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Rent_A_Car_PR_7_2016.Controllers
+{
+    public class VoziloController : Controller
+    {
+        Podaci podaci;
+        // GET: Vozilo
+        public ActionResult Index()
+        {
+            //return View();
+            NapraviSesiju();
+
+            //List<Manifestacija> manifestacije = (List<Manifestacija>)HttpContext.Application["manifestacije"];
+            List<Vozilo> vozila = Podaci.vozila;
+            return View(vozila);
+        }
+
+
+        private void NapraviSesiju()
+        {
+            podaci = (Podaci)Session["Podaci"];
+            if (podaci == null)
+            {
+                podaci = new Podaci();
+                Session["Podaci"] = podaci;
+            }
+
+        }
+
+    }
+}
