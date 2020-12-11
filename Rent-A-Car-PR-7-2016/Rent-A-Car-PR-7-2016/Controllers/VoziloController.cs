d82e082a775a097f7e20fcbfa8445cbbe679e1c2
Marko Sakotic <marcodeee@gmail.com>
2020-12-11 02:42:09 +0100

    feature/filter-search-and-sort-cars

--- Rent-A-Car-PR-7-2016/Rent-A-Car-PR-7-2016/Controllers/VoziloController.cs
+++ Rent-A-Car-PR-7-2016/Rent-A-Car-PR-7-2016/Controllers/VoziloController.cs
@@ -21,6 +21,193 @@ namespace Rent_A_Car_PR_7_2016.Controllers
             return View(vozila);
         }
 
+        public ActionResult SearchByMarka(string marka)
+        {
+            List<Vozilo> vozila = (List<Vozilo>)HttpContext.Application["vozila"];
+            List<Vozilo> markaFilter = new List<Vozilo>();
+
+            if (marka.Equals(""))
+            {
+                ViewBag.vozila = vozila;
+                markaFilter = vozila;
+            }
+            else
+            {
+                foreach (var vozilo in vozila)
+                {
+                    if (vozilo.MarkaVozila.ToString().Contains(marka))
+                    {
+                        markaFilter.Add(vozilo);
+                    }
+                }
+                ViewBag.users = markaFilter;
+            }
+
+            return View("~/Views/Vozilo/Index.cshtml", markaFilter);
+        }
+
+
+        public ActionResult SearchByMesto(string mesto)
+        {
+            List<Vozilo> vozila = (List<Vozilo>)HttpContext.Application["vozila"];
+            List<Vozilo> mestoFilter = new List<Vozilo>();
+
+            if (mesto.Equals(""))
+            {
+                ViewBag.vozila = vozila;
+                mestoFilter = vozila;
+            }
+            else
+            {
+                foreach (var vozilo in vozila)
+                {
+                    if (vozilo.Mesto.ToString().Contains(mesto))
+                    {
+                        mestoFilter.Add(vozilo);
+                    }
+                }
+                ViewBag.users = mestoFilter;
+            }
+
+            return View("~/Views/Vozilo/Index.cshtml", mestoFilter);
+        }
+
+
+        public ActionResult SearchByCena(string cenaod, string cenado)
+        {
+            List<Vozilo> vozila = (List<Vozilo>)HttpContext.Application["vozila"];
+            List<Vozilo> cenaFilter = new List<Vozilo>();
+
+            if (cenaod.Equals(""))
+            {
+                ViewBag.vozila = vozila;
+                cenaFilter = vozila;
+            }
+            else
+            {
+                int cenaood = Int32.Parse(cenaod);
+                foreach (var vozilo in vozila)
+                {
+                    if (vozilo.CenaPoDanu >= cenaood)
+                    {
+                        cenaFilter.Add(vozilo);
+                    }
+                }
+                ViewBag.users = cenaFilter;
+            }
+
+            return View("~/Views/Vozilo/Index.cshtml", cenaFilter);
+        }
+
+        public ActionResult SearchByCenaDo(string cenaod, string cenado)
+        {
+            List<Vozilo> vozila = (List<Vozilo>)HttpContext.Application["vozila"];
+            List<Vozilo> cenaFilter = new List<Vozilo>();
+
+            if (cenado.Equals(""))
+            {
+                ViewBag.vozila = vozila;
+                cenaFilter = vozila;
+            }
+            else
+            {
+                int cenaodo = Int32.Parse(cenado);
+                foreach (var vozilo in vozila)
+                {
+                    if (vozilo.CenaPoDanu <= cenaodo)
+                    {
+                        cenaFilter.Add(vozilo);
+                    }
+                }
+                ViewBag.users = cenaFilter;
+            }
+
+            return View("~/Views/Vozilo/Index.cshtml", cenaFilter);
+        }
+
+        public ActionResult FiltriranjeTipa(string tip)
+        {
+            List<Vozilo> vozila = (List<Vozilo>)HttpContext.Application["vozila"];
+            List<Vozilo> mestoFilter = new List<Vozilo>();
+
+            if (tip.Equals(""))
+            {
+                ViewBag.vozila = vozila;
+                mestoFilter = vozila;
+            }
+            else
+            {
+                foreach (var vozilo in vozila)
+                {
+                    if (vozilo.TipVozila.ToString().Equals(tip))
+                    {
+                        mestoFilter.Add(vozilo);
+                    }
+                }
+                ViewBag.users = mestoFilter;
+            }
+
+            return View("~/Views/Vozilo/Index.cshtml", mestoFilter);
+        }
+
+        public ActionResult Sortiraj(string tipSortiranja)
+        {
+            if (tipSortiranja == "opadajuce")
+            {
+                List<Vozilo> sortirano = new List<Vozilo>();
+
+                List<Vozilo> lista = Podaci.vozila;
+
+                while (lista.Count != 0)
+                {
+                    Vozilo m = lista[0];
+
+                    foreach (Vozilo item in lista)
+                    {
+                        if (item.CenaPoDanu > m.CenaPoDanu)
+                        {
+                            m = item;
+                        }
+                    }
+
+                    sortirano.Add(m);
+                    lista.Remove(m);
+                }
+
+                ViewBag.vozila = sortirano;
+                Podaci.ReadProducts("~/App_Data/vozila.txt");
+                return View("~/Views/Vozilo/Index.cshtml", sortirano);
+            }
+            else if (tipSortiranja == "rastuce")
+            {
+                List<Vozilo> sortirano = new List<Vozilo>();
+
+                List<Vozilo> lista = Podaci.vozila;
+
+                while (lista.Count != 0)
+                {
+                    Vozilo a = lista[0];
+
+                    foreach (Vozilo item in lista)
+                    {
+                        if (item.CenaPoDanu < a.CenaPoDanu)
+                        {
+                            a = item;
+                        }
+                    }
+
+                    sortirano.Add(a);
+                    lista.Remove(a);
+                }
+
+                Podaci.ReadProducts("~/App_Data/vozila.txt");
+                return View("~/Views/Vozilo/Index.cshtml", sortirano);
+
+            }
+
+            return View("~/Views/Vozilo/Index.cshtml");
+        }
+
 
         private void NapraviSesiju()
         {
